8ec61c1fd54c601b07eccc430c5eee6168e38daf
Navneel <navneel99@gmail.com>
2018-05-18 18:30:36 +0530

    Fixed a silly mistake where the user had to explicitly wirte down the name of the app. now, just opening the localhost does the trick.

--- review/urls.py
+++ review/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('inputdata.urls')),
     path('admin/', admin.site.urls),
     path('inputdata/', include('inputdata.urls'))
 ]
